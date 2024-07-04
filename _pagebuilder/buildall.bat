b08cca709623a86d2d406a3f32a4c8ab0d23694f
Sharlikran <danielhmpdx16@gmail.com>
2024-07-03 22:12:18 -0700

    performance

--- _pagebuilder/buildall.bat
+++ _pagebuilder/buildall.bat
@@ -1,3 +1,4 @@
 C:\Python38-32\python.exe wtxt-wiki.py genHtml index.txt
 C:\Python38-32\python.exe wtxt-wiki.py genHtml 1-workplace.txt
 C:\Python38-32\python.exe wtxt-wiki.py genHtml 2-concern.txt
+C:\Python38-32\python.exe wtxt-wiki.py genHtml 3-performance.txt
